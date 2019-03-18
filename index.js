49de17183a65052cf1a97626586e4b969c31effb
Alec <alec.jordan.cs@gmail.com>
2019-03-17 23:18:43 -0400

    figured out basic routing without express

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const http = require('http');
 const express = require('express');
-const server = express()
+// const server = express()
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
-
-server.get('/test', (req, res) => {
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Jahti');
-})
+  res.end(req.url);
+});
+
+// server.get('/test', (req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Jahti');
+// })
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
